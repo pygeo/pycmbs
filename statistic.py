a350a654b54cec290e4509e8fb35c34fe10d8391
alexander.loew <alexander.loew@46f1e1ae-ddaa-4461-89cc-888b818d8ef3>
2012-03-20 15:10:49 +0000

    git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@3 46f1e1ae-ddaa-4461-89cc-888b818d8ef3

---
+++ statistic.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = "Alexander Loew"
+__version__ = "0.0"
+__date__ = "0000/00/00"
+
+
+from scipy import stats
+import numpy as np
+
+def get_significance(correlation,n,pthres=1.01):
+    """
+    calculate significance of correlation
+    
+    @param correlation: pearson correlation coefficient
+    @type  correlation: float
+    
+    @param n: number of samples
+    @type  n: integer
+    
+    @param pthres: (optional) specifies threshold for p-value. Everything
+        above this threshold will be masked
+    @type  n: integer
+    
+    @return: returns the p-value
+    @rtype : numpy masked array
+    
+    """
+    
+    nf = n - 2. #degree of freedom
+    t_value = np.abs(correlation) * np.sqrt( nf / (1.-correlation**2)) # abs() is important
+    
+    # calculate two-sided p-value
+    p =   2. * stats.t.sf(t_value,nf)
+    
+    
+    
+    
+    
+    return np.ma.array(p,mask=p>pthres)
