333e06d2dd1a8d83213ff0f604918dff03cbe675
alexander.loew <alexander.loew@46f1e1ae-ddaa-4461-89cc-888b818d8ef3>
2012-11-15 16:02:44 +0000

    CORE ==== bugfix - diagnostic.py - bugfix in _get_valid_timeseries() for SVD, as the invalid data was not handled properly! bugfix - diagnostic.py - when plotting mupliple EOFs, no new figure was generated. This was fixed now bugfix - data.py       - mask was nor properly set, when no fill value is given in the file. This was fixed now feature - diagnostic.py - added additional option to EOF plot routine to plot normalized correlations instead of original EOF values
    added (non-working) EOF test
    
    git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@84 46f1e1ae-ddaa-4461-89cc-888b818d8ef3

---
+++ tests/test_EOF.py
@@ -0,0 +1,70 @@
+from unittest import TestCase
+from data import *
+from diagnostic import *
+import scipy as sc
+import pylab as pl
+import numpy as np
+
+__author__ = 'm300028'
+
+class TestEOF(TestCase):
+
+    def setUp(self):
+        #init Data object for testing
+        n=4 #slows down significantly! constraint is percentile  test
+        x = sc.randn(n)*100. #generate dummy data
+        self.D = Data(None,None)
+        d=np.ones((n,1,2))
+        self.D.data = d
+        self.D.data[:,0,0]=x
+        self.D.data = np.ma.array(self.D.data,mask=self.D.data != self.D.data)
+        self.D.verbose = True
+        self.D.unit = 'myunit'
+
+        self.D.time = np.arange(n) + pl.datestr2num('2001-01-01') - 1
+
+
+    def test_eof_analysis(self):
+        #test of EOF
+        #example taken from:
+        #   http://www.atmos.washington.edu/~dennis/552_Notes_4.pdf , page 80
+
+        #assign sampled data
+        D = self.D.copy()
+#        d1 = np.array([2,4,-6,8])
+#        d2 = np.array([1,2,-3,4])
+        #d1 = np.array([2,1])
+        #d2 = np.array([4,2])
+        #d3 = np.array([-6,-3])
+        #d4 = np.array([8,4])
+
+        #D.data[:,0,0] = d1; D.data[:,0,1] = d2
+        #D.data[:,0,2] = d3; D.data[:,0,3] = d4
+
+#        D.data[:,0,0] = d1
+#        D.data[:,0,1] = d2
+#
+#        E = EOF(D,cov_norm=False) #do not normalize covariance matrix, as this is also not done in example
+#        print E.C #covariance matrix
+
+
+#        shape of EOF is wrong !!!!!!!
+#
+#        something is not really working here !!!!
+#
+#
+#        irgendwie ist hier ein problem
+#        warum einmal 4x4 und einmal 2x2 ???
+
+#        print 'eigval'
+#        print E.eigval
+#
+#        print 'eigvec'
+#        print E.eigvec
+
+
+
+
+
+
+    pass
