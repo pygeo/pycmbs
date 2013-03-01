b9af21860e1377165e622e4523d13f2d5dcbb4ac
alexander.loew <alexander.loew@46f1e1ae-ddaa-4461-89cc-888b818d8ef3>
2013-03-01 10:56:08 +0000

    git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@144 46f1e1ae-ddaa-4461-89cc-888b818d8ef3

--- report.py
+++ report.py
@@ -125,8 +125,8 @@ class Report():
 
         self.write('\maketitle')
 
-        if self.logofile != None:
-            self._write_single_figure(self.logofile,None) #logo for report
+        #if self.logofile != None:
+        #    self._write_single_figure(self.logofile,None) #logo for report
 
 
         self.write('\\newpage')
