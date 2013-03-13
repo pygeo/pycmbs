CORE ==== bugfix - plots.py: bugfix for Gleckler plot. The error was calculated as E**2 instead of E=RMSE. This ha caused a wrong calculation of the mean relative errors. This is now fixed                    MAJOR bugfix
git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@202 46f1e1ae-ddaa-4461-89cc-888b818d8ef3

tests/test_diagnostic.py
@@ -50,11 +50,11 @@ class TestData(TestCase):
         #--- diagnostic ---
         D = GlecklerPlot()
         r=D.calc_index(x,y,'a','b')
-        self.assertEqual(r,22.5)
+        self.assertEqual(r,np.sqrt(22.5))
 
         #... use different std
         x.std  = np.ones(x.data.shape)
         x.std[:,2,0] = 0.5
         r=D.calc_index(x,y,'a','b')
-        self.assertEqual(r,42.5)
+        self.assertEqual(r,np.sqrt(42.5))
 