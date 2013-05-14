e56f3b622b8c3e818fb4bd9076ecc723bbef5807
Alexander Loew <alexander.loew@mpimet.mpg.de>
2013-05-14 19:18:10 +0000

    CORE ==== bugfix - plots.py: small bugfix for GlecklerPlot, that values are not plotted in legend feature - plots.py: added option to provide colormap object to map_plot
    git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@221 46f1e1ae-ddaa-4461-89cc-888b818d8ef3

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(name='pyCMBS',
+      version='0.0.1',
+      py_modules=[],author='Alexander Loew',
+      author_email='alexander.loew@zmaw.de',
+      maintainer='Alexander Loew',
+      maintainer_email='alexander.loew@zmaw.de',
+      url='https://code.zmaw.de/projects/pycmbs',
+      description='pyCMBS - python Climate Model Benchmarking Suite',
+      long_description='The pyCMBS project is a suite of tools to process, analyze, visualize and benchmark scientific model output against each other or against observational data. It is in particular useful for analyzing in an efficient way output from climate model simulations.'
+      )
+
+
+#todo:
+#- set PYTHONPATH to directory, where user installs pyCMBS, sys.path ???
+
