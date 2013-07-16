18f3442973dbbf2592f22d1b9ba39d03beb83233
alexander.loew <alexander.loew>
2013-07-16 09:41:43 +0000

    minor update
    git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@259 46f1e1ae-ddaa-4461-89cc-888b818d8ef3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import os
 
 setup(name='pyCMBS',
-      version='0.1.0',
+      version='0.1.1',
       py_modules=[],author='Alexander Loew',
       author_email='alexander.loew@zmaw.de',
       maintainer='Alexander Loew',
