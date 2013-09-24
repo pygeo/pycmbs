31963cfa431ae7db6ff676074e2115cdcdcf9670
alexander.loew <alexander.loew>
2013-09-24 14:38:54 +0000

    git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@305 46f1e1ae-ddaa-4461-89cc-888b818d8ef3

--- plots.py
+++ plots.py
@@ -834,7 +834,7 @@ class GlobalMeanPlot():
 
             ax.fill_between(tval,ym-std_data,ym+std_data,color=color,alpha=0.5)
             ax.plot(tval,ym,label=g+'$\pm 1\sigma$',color=color)
-            ax.set_ylabel(dat['unit'])
+            ax.set_ylabel(dat[0]['unit'])
             ax.set_xlabel('months')
 
         ax.legend()
