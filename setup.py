bc710c6184fe3725c7336b292f43aafd422fc37f
Alexander Loew <alexander.loew@mpimet.mpg.de>
2013-11-24 21:48:34 +0000

    Initial version with running setup.py install to allow for automated installation of pyCMBS; todo: modification of system pathes
    git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@437 46f1e1ae-ddaa-4461-89cc-888b818d8ef3
    
    Former-commit-id: 9389a99712485b94e9ba969b30a54c2ee1f22b46

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 import os
 
+import pyCMBS
 
 install_requires=["numpy>0.1","cdo>1.2",]
 
 
 setup(name='pyCMBS',
-      version='0.1.4-rev',
+      version=pyCMBS.__version__,
       py_modules=['pyCMBS',],
+      packages=['pyCMBS','pyCMBS/framework','pyCMBS/tests','pyCMBS/logo','pyCMBS/misc','pyCMBS/examples','pyCMBS/tools'],
       author = "Alexander Loew",
       author_email='alexander.loew@mpimet.mpg.de',
       maintainer='Alexander Loew',
@@ -16,7 +18,9 @@ setup(name='pyCMBS',
       description='pyCMBS - python Climate Model Benchmarking Suite',
       long_description='The pyCMBS project is a suite of tools to process, analyze, visualize and benchmark scientific model output against each other or against observational data. It is in particular useful for analyzing in an efficient way output from climate model simulations.',
       install_requires=install_requires,
-      license = ""      )
+      keywords = ["data","science","climate","meteorology","model evaluation","benchmarking","metrics"],
+      scripts = ["pyCMBS/framework/pycmbs.py"],
+      license = "XXXX"      )
 
 #print('Compressing sample data ...')
 #ofile='./dist/example_data.tgz'
@@ -26,45 +30,15 @@ setup(name='pyCMBS',
 #os.system(cmd)
 
 
-print ''
-print 'Your original PYTHONPATH is:'
-print os.environ['PYTHONPATH']
-print''
-print 'Please change your PYTHONPATH (permanently) in your shell configuration file as follows:'
-print ''
-print 'For BASH shell users include in your .bashrc:'
-curdir = os.getcwd()
-print '     export PYTHONPATH=' + curdir + ':$PYTHONPATH'
-print '     export SEP="/pool/SEP"'
-print 'The latter is only for users at ZMAW'
-
-
-#todo:
-#- set PYTHONPATH to directory, where user installs pyCMBS, sys.path ???
-#
-#what to do with the TAR-file
-#1) download and extract it
-#2) rename directory with version number to 'pyCMBS'
-#3) put pyCMBS directory in your path
 
 
 
 #PIP
-#~ py_modules --> remote identifier ???
-#~
-#~
 #~ python setup.py register
 #~ python setup.py sdist
 #~ python setup.py upload
 
 
-
-
-#python GMT !!!
-
-
-
-#ARcLinux ???
 
 
 
