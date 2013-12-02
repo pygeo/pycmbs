16fdd448b453ed6f4aaa6266ba5a451ad0158628
Alexander Loew <alexander.loew@mpimet.mpg.de>
2013-12-02 16:03:23 +0000

    made icon.py PEP8 compliant
    git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@441 46f1e1ae-ddaa-4461-89cc-888b818d8ef3
    
    Former-commit-id: 98e3ff562ee877407c8a3471d574139c263076f6

--- pyCMBS/icon.py
+++ pyCMBS/icon.py
@@ -1,22 +1,9 @@
-__author__ = "Alexander Loew"
-__version__ = "0.1.4"
-__date__ = "2013/06/30"
-__email__ = "alexander.loew@mpimet.mpg.de"
-
+# -*- coding: utf-8 -*-
 """
-Copyright (C) 2012 Alexander Loew, alexander.loew@mpimet.mpg.de
-See COPYING file for copying and redistribution conditions.
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2 of the License.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+This file is part of pyCMBS.
+For COPYRIGHT, LICENSE and AUTHORSHIP please referr to
+the pyCMBS licensing details.
 """
-
 
 from pyCMBS.data import Data
 import os
@@ -29,54 +16,70 @@ class Icon(Data):
     Main class for ICON data handling
     """
 
-    def __init__(self,filename,gridfile,varname,read=False,**kwargs):
+    def __init__(self, filename, gridfile, varname, read=False, **kwargs):
         """
-        @param filename: filename of data file
-        @type filename : str
+        Parameters
+        ----------
 
-        @param gridfile: filename of grid definition file
-        @type gridfile: str
+        filename : str
+            filename of data file
 
-        @param varname: name of variable to handle
-        @type varname: str
+        gridfile : str
+            filename of grid definition file
+
+        varname : str
+            name of variable to handle
+
+        read : bool
+            specify if data should be read immediately
         """
-        Data.__init__(self,filename,varname,**kwargs)
+        Data.__init__(self, filename, varname, **kwargs)
         self.gridfile = gridfile
-
         self.gridtype = 'unstructured'
 
 #---
 
-    def read(self,time_var='time'):
+    def read(self, time_var='time'):
         """
         This is a special routine for reading data from ICON structure
         a bit redundant to Data.read()
+
+        Parameters
+        ----------
+
+        time_var : str
+            name of time variable (default='time')
         """
-        print 'Reading ICON data ...'
+        print('Reading ICON data ...')
 
         if not os.path.exists(self.filename):
-            raise ValueError, 'File not existing: ' + self.filename
+            raise ValueError('File not existing: %s' % self.filename)
         if not os.path.exists(self.gridfile):
-            raise ValueError, 'File not existing: ' + self.gridfile
+            raise ValueError('File not existing: %s' % self.gridfile)
 
         #--- time variable
         self.time_var = time_var
 
         #--- data field
-        self.data = self.read_netcdf(self.varname) #[time,ncell]
-        nt,ncell = self.data.shape
-        self.data = self.data.reshape((nt,1,ncell)) #reshape so we have a common 3D structure like always in pyCMBS
-        if self.data == None:
-            raise ValueError, 'The data in the file ' + self.filename + ' is not existing. This must not happen!'
-        if self.scale_factor == None:
-            raise ValueError, 'The scale_factor for file ' + self.filename + 'is NONE, this must not happen!'
+        # [time,ncell]
+        self.data = self.read_netcdf(self.varname)
+        nt, ncell = self.data.shape
+        # reshape so we have a common 3D structure like always in pyCMBS
+        self.data = self.data.reshape((nt, 1, ncell))
+        if self.data is None:
+            raise ValueError('The data in the file %s is not existing. \
+                            This must not happen!' % self.filename)
+        if self.scale_factor is None:
+            raise ValueError('The scale_factor for file %s is NONE, \
+                              this must not happen!' % self.filename)
 
-        self.data = self.data * self.scale_factor
+        self.data *= self.scale_factor
 
         #--- read lat/lon
-        File=NetCDFHandler()
-        File.open_file(self.gridfile,'r')
-        self.lon = File.get_variable('clon')*180./np.pi #grid cell center coordinates
+        File = NetCDFHandler()
+        File.open_file(self.gridfile, 'r')
+        # grid cell center coordinates
+        self.lon = File.get_variable('clon')*180./np.pi
         self.lat = File.get_variable('clat')*180./np.pi
         self.ncell = len(self.lon)
 
@@ -86,23 +89,20 @@ class Icon(Data):
         File.close()
 
         #--- read time variable
-        if self.time_var != None:
-            self.time = self.read_netcdf(self.time_var) #returns either None or a masked array
-            if hasattr(self.time,'mask'):
+        if self.time_var is not None:
+            # returns either None or a masked array
+            self.time = self.read_netcdf(self.time_var)
+            if hasattr(self.time, 'mask'):
                 self.time = self.time.data
             else:
-                self.time = None
-            if self.time != None:
+                self.time is None
+            if self.time is not None:
                 if self.time.ndim != 1:
-                    self.time = self.time.flatten() #remove singletone dimensions
+                    # remove singletone dimensions
+                    self.time = self.time.flatten()
         else:
             self.time = None
 
         #--- determine time --> convert to python timestep
-        if self.time != None:
+        if self.time is not None:
             self.set_time()
-
-
-
-
-
