2c90c7b9c7a3ac7b69ad001b16576538c1958789
alexander.loew <alexander.loew>
2013-12-02 16:03:23 +0000

    made netcdf.py PEP8 compliant
    git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@442 46f1e1ae-ddaa-4461-89cc-888b818d8ef3
    
    Former-commit-id: 981dfbef24770c22a628a244ca0b6ff89c3c502c

--- pyCMBS/netcdf.py
+++ pyCMBS/netcdf.py
@@ -1,9 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+This file is part of pyCMBS.
+For COPYRIGHT, LICENSE and AUTHORSHIP please referr to
+the pyCMBS licensing details.
+"""
+
 """
 This module allows a flexible choice of the netCDF backend
 """
+# specify backend type
+netcdf_backend = 'netCDF4'
+#~ netcdf_backend = 'Nio'
 
-netcdf_backend='netCDF4'
-#~ netcdf_backend='Nio'
 
 class NetCDFHandler(object):
     def __init__(self):
@@ -13,31 +21,44 @@ class NetCDFHandler(object):
         elif self.type.lower() == 'netcdf4':
             import netCDF4 as Cdf
         else:
-            raise ValueError, 'Invalid netCDF backend!'
+            raise ValueError('Invalid netCDF backend!')
         self.handler = Cdf
 
-    def open_file(self,filename,mode):
+    def open_file(self, filename, mode):
         """
+        Open a netCDF file using the predefined backend
+
+        Parameters
+        ----------
+        filename : str
+            name of file to read
+        mode : str
+            specify read or write data access ['w','r']
+
         Returns
         -------
         F : file handler
             returns a file handler
         """
-        if mode not in ['w','r']:
-            raise ValueError, 'Invalid mode!'
+        if mode not in ['w', 'r']:
+            raise ValueError('Invalid mode! [w,r], %s' % mode)
         if self.type.lower() == 'nio':
-            self.F = self.handler.open_file(filename,mode=mode)
+            self.F = self.handler.open_file(filename, mode=mode)
             self.create_dimension = self.F.create_dimension
-            self.create_variable  = self.F.create_variable
+            self.create_variable = self.F.create_variable
         else:
-            self.F = self.handler.Dataset(filename,mode=mode)
+            self.F = self.handler.Dataset(filename, mode=mode)
             self.create_dimension = self.F.createDimension
-            self.create_variable  = self.F.createVariable
+            self.create_variables = self.F.createVariable
 
-
-    def get_variable(self,varname):
+    def get_variable(self, varname):
         """
         Get data for a particular variable
+
+        Parameters
+        ----------
+        varname : str
+            variable name of the netcdf variable to read
 
         Returns
         -------
@@ -49,7 +70,7 @@ class NetCDFHandler(object):
         else:
             return self.F.variables[varname][:].astype('float').copy()
 
-    def get_variable_handler(self,varname):
+    def get_variable_handler(self, varname):
         """
         Get handler to a variable
 
@@ -62,7 +83,7 @@ class NetCDFHandler(object):
         else:
             return self.F.variables[varname]
 
-    def _get_scale_factor(self,varname):
+    def _get_scale_factor(self, varname):
         if self.type.lower() == 'nio':
             try:
                 return float(self.F.variables[varname].scale_factor)
@@ -73,7 +94,7 @@ class NetCDFHandler(object):
             #netCDF4 library already applies the scaling factor!
             return 1.
 
-    def _get_add_offset(self,varname):
+    def _get_add_offset(self, varname):
         if self.type.lower() == 'nio':
             try:
                 return float(self.F.variables[varname].add_offset)
@@ -84,8 +105,7 @@ class NetCDFHandler(object):
             #netCDF4 library already applies the add_offset!
             return 0.
 
-
-    def assign_value(self,varname,value):
+    def assign_value(self, varname, value):
         """
         assign a value to a variable to be written to a netCDF file
         """
@@ -94,9 +114,5 @@ class NetCDFHandler(object):
         else:
             self.F.variables[varname][:] = value[:]
 
-
     def close(self):
         self.F.close()
-
-
-
