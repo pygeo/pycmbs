0fda04e2810dfe859462a4b6d5d0da3310b53961
alexander.loew <alexander.loew@46f1e1ae-ddaa-4461-89cc-888b818d8ef3>
2013-12-05 07:00:45 +0000

    Cleaned up some of the code to be PEP8 compliant
    git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@465 46f1e1ae-ddaa-4461-89cc-888b818d8ef3
    
    Former-commit-id: 0b89101b94fd02b4c04255bb2a4ef809aa0022af
    Former-commit-id: 78c19b0e5cf9c7c2a8ceea37a2b3cc393311ca09

--- pyCMBS/diagnostic.py
+++ pyCMBS/diagnostic.py
@@ -2474,13 +2474,16 @@ class koeppen(object):
      nt_p, ny_p, nx_p = self.precip.data.data.shape
      ny_l, nx_l = self.lsm.data.data.shape
 
-
      if (ny_t != ny_p) or (ny_t != ny_l):
-       sys.exit('ERROR: The resolution ot the three arrays differ in Y-dimension: \n' + str(ny_t)+ "(temp)  " + str(ny_p) + "(precip) " + str(ny_l) + "(lsm) ")
+       sys.exit('ERROR: The resolution ot the three arrays differ in \
+       Y-dimension: \n' + str(ny_t)+ "(temp)  " + str(ny_p)
+       + "(precip) " + str(ny_l) + "(lsm) ")
        return False
 
      if (nx_t != nx_p) or (nx_t != nx_l):
-       sys.exit('ERROR: The resolution ot the three arrays differ in X-dimension: \n' + str(nx_t)+ "(temp)  " + str(nx_p) + "(precip) " + str(nx_l) + "(lsm) ")
+       sys.exit('ERROR: The resolution ot the three arrays differ in \
+       X-dimension: \n' + str(nx_t)+ "(temp)  " + str(nx_p)
+       + "(precip) " + str(nx_l) + "(lsm) ")
        return False
 
      return True
@@ -2564,16 +2567,16 @@ class koeppen(object):
         Pmax = self.precip.data.max(axis=0)
 
         precipHS = self.precip.copy()
-        precipHS.data[(0,1,2,3,4,5),0:(nlat/2-1),:]  = self.precip.data[(3,4,5,6,7,8),0:(nlat/2-1),:]
-        precipHS.data[(6,7,8,9,10,11),0:(nlat/2-1),:] = self.precip.data[(3,4,5,6,7,8),0:(nlat/2-1),:]
-        precipHS.data[(0,1,2,3,4,5),(nlat/2):(nlat-1),:]   = self.precip.data[(0,1,2,9,10,11),(nlat/2):(nlat-1),:]
-        precipHS.data[(6,7,8,9,10,11),(nlat/2):(nlat-1),:] = self.precip.data[(0,1,2,9,10,11),(nlat/2):(nlat-1),:]
+        precipHS.data[(0, 1, 2, 3, 4, 5), 0:(nlat/2-1), :] = self.precip.data[(3, 4, 5, 6, 7, 8),0:(nlat/2-1), :]
+        precipHS.data[(6, 7, 8, 9, 10, 11), 0:(nlat/2-1), :] = self.precip.data[(3, 4, 5, 6, 7, 8),0:(nlat/2-1), :]
+        precipHS.data[(0, 1, 2, 3, 4, 5), (nlat/2):(nlat-1), :]   = self.precip.data[(0, 1, 2, 9, 10, 11),(nlat/2):(nlat-1), :]
+        precipHS.data[(6, 7, 8, 9, 10, 11),(nlat/2):(nlat-1), :] = self.precip.data[(0, 1, 2, 9, 10, 11),(nlat/2):(nlat-1), :]
 
         precipHW = self.precip.copy()
-        precipHW.data[(0,1,2,3,4,5),0:(nlat/2-1),:]   = self.precip.data[(0,1,2,9,10,11),0:(nlat/2-1),:]
-        precipHW.data[(6,7,8,9,10,11),0:(nlat/2-1),:] = self.precip.data[(0,1,2,9,10,11),0:(nlat/2-1),:]
-        precipHW.data[(0,1,2,3,4,5),(nlat/2):(nlat-1),:]  = self.precip.data[(3,4,5,6,7,8),(nlat/2):(nlat-1),:]
-        precipHW.data[(6,7,8,9,10,11),(nlat/2):(nlat-1),:] = self.precip.data[(3,4,5,6,7,8),(nlat/2):(nlat-1),:]
+        precipHW.data[(0,1,2,3,4,5),0:(nlat/2-1),:]   = self.precip.data[(0, 1, 2, 9, 10, 11),0:(nlat/2-1),:]
+        precipHW.data[(6,7,8,9,10,11),0:(nlat/2-1),:] = self.precip.data[(0, 1, 2, 9, 10, 11),0:(nlat/2-1),:]
+        precipHW.data[(0,1,2,3,4,5),(nlat/2):(nlat-1),:]  = self.precip.data[(3, 4, 5, 6, 7, 8),(nlat/2):(nlat-1),:]
+        precipHW.data[(6,7,8,9,10,11),(nlat/2):(nlat-1),:] = self.precip.data[(3, 4, 5, 6, 7, 8),(nlat/2):(nlat-1),:]
 
         PminHS = precipHS.data.min(axis=0)   # Bestimmt den minimalen Monastniederschlag aus PmaxHS
         PmaxHS = precipHS.data.max(axis=0)   # Bestimmt den maximalen Monastniederschlag aus PmaxHS
@@ -2585,117 +2588,117 @@ class koeppen(object):
         Tmax = self.temp.data.max(axis=0)     # Bestimmt die maximale Jahrestemperatur
 
         self.Clim = self.precip.timmean(return_object=True)
-
         self.Clim.units = "climate type"
 
         for lat in range(0, nlat):
           for lon in range(0, nlon):
-            psum   = Psum.data.data[lat][lon]
-            pmin   = Pmin[lat][lon]
+            psum = Psum.data.data[lat][lon]
+            pmin = Pmin[lat][lon]
             pminhs = PminHS[lat][lon]
             pminhw = PminHW[lat][lon]
             pmaxhs = PmaxHS[lat][lon]
             pmaxhw = PmaxHW[lat][lon]
-            tavg   = Tavg[lat][lon]
-            tmin   = Tmin[lat][lon]
-            tmax   = Tmax[lat][lon]
-            self.Clim.data.data[lat][lon] = self.set_clim(psum,pmin,pminhs,pminhw,pmaxhs,pmaxhw,tavg,tmin,tmax)
+            tavg = Tavg[lat][lon]
+            tmin = Tmin[lat][lon]
+            tmax = Tmax[lat][lon]
+            self.Clim.data.data[lat][lon] = self.set_clim(psum, pmin, pminhs, pminhw, pmaxhs, pmaxhw, tavg, tmin, tmax)
 
-
-        self.Clim.data.mask[less(self.lsm.data,0.5)]=True
+        self.Clim.data.mask[less(self.lsm.data, 0.5)]=True
 
 #-----------------------------------------------------------------------------------------------------------------------
 
     def copy(self):
-      """
-          Returns the Clim Data as an Data variable
-      """
-      return self.Clim
+        """
+        Returns the Clim Data as an Data variable
+        """
+        return self.Clim
 
 #-----------------------------------------------------------------------------------------------------------------------
 
     def climfrac(self):
-      """
-          This routine calculats the fraction of each type in per centum.
-          ToDo:
-            Unclear id the print is OK or if the values should given back as an array.
-      """
-      climfrac = [0] * 14
+        """
+        This routine calculats the fraction of each type in per centum.
+        ToDo:
+        Unclear id the print is OK or if the values should given back as an array.
+        """
+        climfrac = [0] * 14
 
-      ny,nx = self.Clim.data.data.shape
-      for ny in range(0, ny-1):
-        Aweight = cos(self.Clim.lat[ny][0] / 180 * 3.14159265359)
-        for nx in range(0, nx-1):
-          clim = int(self.Clim.data.data[ny][nx])
-          climfrac[clim-1] = climfrac[clim-1] + Aweight
+        ny, nx = self.Clim.data.data.shape
+        for ny in range(0, ny-1):
+            Aweight = cos(self.Clim.lat[ny][0] / 180 * 3.14159265359)
+            for nx in range(0, nx-1):
+                clim = int(self.Clim.data.data[ny][nx])
+                climfrac[clim-1] = climfrac[clim-1] + Aweight
 
-      s = sum(climfrac)
-      climfrac[:] = [x / s for x in climfrac]
+        s = sum(climfrac)
+        climfrac[:] = [x / s for x in climfrac]
 
-
-      print "Af: " + str(climfrac[0])
-      print "Am: " + str(climfrac[1])
-      print "As: " + str(climfrac[2])
-      print "Aw: " + str(climfrac[3])
-      print "BS: " + str(climfrac[4])
-      print "BW: " + str(climfrac[5])
-      print "Cf: " + str(climfrac[6])
-      print "Cs: " + str(climfrac[7])
-      print "Cw: " + str(climfrac[8])
-      print "Df: " + str(climfrac[9])
-      print "Ds: " + str(climfrac[10])
-      print "Dw: " + str(climfrac[11])
-      print "ET: " + str(climfrac[12])
-      print "EF: " + str(climfrac[13])
+        print "Af: " + str(climfrac[0])
+        print "Am: " + str(climfrac[1])
+        print "As: " + str(climfrac[2])
+        print "Aw: " + str(climfrac[3])
+        print "BS: " + str(climfrac[4])
+        print "BW: " + str(climfrac[5])
+        print "Cf: " + str(climfrac[6])
+        print "Cs: " + str(climfrac[7])
+        print "Cw: " + str(climfrac[8])
+        print "Df: " + str(climfrac[9])
+        print "Ds: " + str(climfrac[10])
+        print "Dw: " + str(climfrac[11])
+        print "ET: " + str(climfrac[12])
+        print "EF: " + str(climfrac[13])
 
 #-----------------------------------------------------------------------------------------------------------------------
 
     def legend(self):
-      """
-          This routine prints a legend of the geiger-köppen types.
-          The description is taken from:
-            MARKUS KOTTEK, JÜRGEN GRIESER, CHRISTOPH BECK , BRUNO RUDOLF and FRANZ RUBEL
-              World Map of the Köppen-Geiger climate classiﬁcation updated
-              Meteorologische Zeitschrift, Vol. 15, No. 3, 259-263 (June 2006)
+        """
+        This routine prints a legend of the geiger-köppen types.
+        The description is taken from:
+        MARKUS KOTTEK, JÜRGEN GRIESER, CHRISTOPH BECK , BRUNO RUDOLF and FRANZ RUBEL
+        World Map of the Köppen-Geiger climate classiﬁcation updated
+        Meteorologische Zeitschrift, Vol. 15, No. 3, 259-263 (June 2006)
 
-      """
+        """
 
-
-      print "|================= Class legend =================|"
-      print "| Af: Equatorial rainforest, fully humid         |"
-      print "| Am: Equatorial climates                        |"
-      print "| As: Equatorial monsoon                         |"
-      print "| Aw: Equatorial savannah with dry winter        |"
-      print "|------------------------------------------------|"
-      print "| BS: Steppe climate                             |"
-      print "| BW: Desert climate                             |"
-      print "|------------------------------------------------|"
-      print "| Cf: Warm temperate climate, fully humid        |"
-      print "| Cs: Warm temperate climate with dry summer     |"
-      print "| Cw: Warm temperate climate with dry winter     |"
-      print "|------------------------------------------------|"
-      print "| Df: Snow climate, fully humid                  |"
-      print "| Ds: Snow climate with dry summer               |"
-      print "| Dw: Snow climate with dry winter               |"
-      print "|------------------------------------------------|"
-      print "| ET: Tundra climate                             |"
-      print "| EF: Frost climate                              |"
-      print "|================================================|"
+        print "|================= Class legend =================|"
+        print "| Af: Equatorial rainforest, fully humid         |"
+        print "| Am: Equatorial climates                        |"
+        print "| As: Equatorial monsoon                         |"
+        print "| Aw: Equatorial savannah with dry winter        |"
+        print "|------------------------------------------------|"
+        print "| BS: Steppe climate                             |"
+        print "| BW: Desert climate                             |"
+        print "|------------------------------------------------|"
+        print "| Cf: Warm temperate climate, fully humid        |"
+        print "| Cs: Warm temperate climate with dry summer     |"
+        print "| Cw: Warm temperate climate with dry winter     |"
+        print "|------------------------------------------------|"
+        print "| Df: Snow climate, fully humid                  |"
+        print "| Ds: Snow climate with dry summer               |"
+        print "| Dw: Snow climate with dry winter               |"
+        print "|------------------------------------------------|"
+        print "| ET: Tundra climate                             |"
+        print "| EF: Frost climate                              |"
+        print "|================================================|"
 
 
 #-----------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
 
     def plot(self, **kwargs):
-      """
-          This routine plots the data of his own geiger-koeppen data by using the plot-routine map_plot.
-          It use the own created color-map and sets the color-bar to a horizontal orientation.
-          It set the range of values between 0.5 and 14.5. Which are the possible values of geiger-köppen.
-          ToDo:
-            At the moment the label of the geiger-köppen types are missing at the color-bar
-      """
-      map_plot(self.Clim, cmap_data=self.cmap, colorbar_orientation='horizontal', vmin=0.5, vmax=14.5,
-         cticks=[1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14.],
-         cticklabels = ["Af", "Am", "As", "Aw", "BS", "BW", "Cf",
-                        "Cs", "Cw", "Df", "Ds", "Dw", "ET", "EF"],
-                        nclasses=15, **kwargs)
+        """
+        This routine plots the data of his own geiger-koeppen data by
+        using the plot-routine map_plot.
+        It use the own created color-map and sets the color-bar to a
+        horizontal orientation.
+        It set the range of values between 0.5 and 14.5. Which are the
+        possible values of geiger-köppen.
+        ToDo:
+        At the moment the label of the geiger-köppen types are missing
+        at the color-bar
+        """
+        map_plot(self.Clim, cmap_data=self.cmap, colorbar_orientation='horizontal', vmin=0.5, vmax=14.5,
+        cticks=[1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14.],
+                cticklabels = ["Af", "Am", "As", "Aw", "BS", "BW", "Cf",
+                       "Cs", "Cw", "Df", "Ds", "Dw", "ET", "EF"],
+                       nclasses=15, **kwargs)
