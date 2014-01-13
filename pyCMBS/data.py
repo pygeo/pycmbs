[data] BUGFIX in timvar() calculation
git-svn-id: https://svn.zmaw.de/svn/pycmbs/trunk@521 46f1e1ae-ddaa-4461-89cc-888b818d8ef3

Former-commit-id: 1aa41ec3c408f0b25bd70e3dc543eb78919327f7
Former-commit-id: 5b87785a1ea18d8909a4902ca5b67aa9cd0d3c2d

pyCMBS/data.py
@@ -2470,10 +2470,9 @@ class Data(object):
         """
         if self.data.ndim == 3:
             res = self.data.var(axis=0)
-        if self.data.ndim == 2:
+        elif self.data.ndim == 2:
             # no temporal averaging
             res = None
-        else:
             sys.exit('Temporal variance can not be calculated as dimensions do not match!')
 
         if return_object: