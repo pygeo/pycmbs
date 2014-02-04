ffe961e2fb138eed9a0d67ebdd956f62ebddb8f7
Mikhail Itkin <itkin@driftnoise.com>
2014-02-04 09:00:04 +0100

    rearranged tests
    Former-commit-id: 56fb06ed92f00c1a4d14cdee08368e2b16a0ff16
    Former-commit-id: db4ba3fdb758ef801e85f2d286647a3bc3c49fbb

--- pycmbs/tests/test_statistic.py
+++ pycmbs/tests/test_statistic.py
@@ -1,11 +1,9 @@
-from unittest import TestCase
+import unittest
 
-__author__ = 'm300028'
-
-from pyCMBS.statistic import *
-
-class TestStatistic(TestCase):
+class TestStatistic(unittest.TestCase):
     def test_get_significance(self):
-        #test get_significance routine
+        from pycmbs.statistic import get_significance
         self.assertAlmostEqual(get_significance(0.5,100.),1.18049202704e-07,delta=1.e-6)
 
+if __name__ == "__main__":
+    unittest.main()
