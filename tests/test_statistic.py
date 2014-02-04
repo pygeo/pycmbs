b84a6568adb49a4f9afb44c5e54c0b469565bae0
Mikhail Itkin <itkin@driftnoise.com>
2014-02-04 08:58:57 +0100

    [tests] rearrranged tests
    Former-commit-id: 78a0f4f1a0095091c0d5aeff32a7bba618b4a395
    Former-commit-id: 1c91244d1a66cd5ad3980827c51ed9bb77694012

--- tests/test_statistic.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestStatistic(unittest.TestCase):
-    def test_get_significance(self):
-        from pycmbs.statistic import get_significance
-        self.assertAlmostEqual(get_significance(0.5,100.),1.18049202704e-07,delta=1.e-6)
-
-if __name__ == "__main__":
-    unittest.main()
