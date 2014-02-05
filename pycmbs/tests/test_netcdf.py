a8a8598ed18db25c0817aff1072a7e5c15080c97
Mikhail Itkin <itkin.m@gmail.com>
2014-02-05 19:16:22 +0100

    [tests/netcdf] dummy test for pycmbs.netcdf
    Former-commit-id: a7170a0196a733fda22d7f905ac4995a92053f07

---
+++ pycmbs/tests/test_netcdf.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from pycmbs import netcdf
+
+class TestPycmbsNetcdf(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_DummyTest(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+
+# vim: expandtab shiftwidth=4 softtabstop=4
