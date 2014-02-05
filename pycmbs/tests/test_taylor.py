041d7c64b349a7be69a275f059253db3dd7d4b9b
Mikhail Itkin <itkin.m@gmail.com>
2014-02-05 13:00:01 +0100

    [tests/taylor] added stub unittest for taylor
    Former-commit-id: 64810e36b6459df323a2a7dc7e1706c34639828c
    Former-commit-id: 2964e49ad762518ea736b1f27b9d01601b51d834

---
+++ pycmbs/tests/test_taylor.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import unittest
+from pycmbs import taylor
+
+class TestTaylor(unittest.TestCase):
+    def test_description(self):
+        pass
+        
+if __name__ == "__main__":
+    unittest.main()
+# vim: expandtab shiftwidth=4 softtabstop=4
