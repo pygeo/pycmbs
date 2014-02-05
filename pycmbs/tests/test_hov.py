2ceb4edfca876fd288d850f85dc24260901f76f4
Mikhail Itkin <itkin.m@gmail.com>
2014-02-05 12:54:12 +0100

    [pycmbs/tests] added test for mapping.py and hov.py
    Former-commit-id: 8da4f05644cef1efeed90829553f2ad439f7229d

---
+++ pycmbs/tests/test_hov.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import unittest
+from pycmbs import hov
+
+class TestHov(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_StubTest(self):
+        self.assertEqual(1, 1)
+        
+if __name__ == "__main__":
+    unittest.main()
+
+# vim: expandtab shiftwidth=4 softtabstop=4
