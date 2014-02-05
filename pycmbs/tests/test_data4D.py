398127c1557a7963f7096529c366504a4426411b
Mikhail Itkin <itkin.m@gmail.com>
2014-02-05 19:42:59 +0100

    [tests/data4D] added dummy test for pycmbs/data4D.py
    Former-commit-id: d2875f60c26acfa1f8ce97b8776890dd26e92154
    Former-commit-id: ab4360516ec4d7408f51d64f8f36450af5ff370e

---
+++ pycmbs/tests/test_data4D.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+from pycmbs import data4d
+
+class TestPycmbsData4D(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_DummyTest(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+# vim: expandtab shiftwidth=4 softtabstop=4
