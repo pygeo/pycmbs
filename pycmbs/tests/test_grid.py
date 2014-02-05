a343779d3fd230b74a49bfbca9ee32e8b4ba1f29
Mikhail Itkin <itkin.m@gmail.com>
2014-02-05 19:32:50 +0100

    [tests/grid] added dummy test for pycmbs/grid.py
    Former-commit-id: 8bc248f7190bacabc8921fbafadee912d06dbed3
    Former-commit-id: 89ab6d10759ac65a1616af1a19ea3527d8cea06d

---
+++ pycmbs/tests/test_grid.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+from pycmbs import grid
+
+class TestPycmbsGrid(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_DummyTest(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+
+# vim: expandtab shiftwidth=4 softtabstop=4
