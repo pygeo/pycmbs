669753d2682b33fe46966ac6a52fc0a6be08854b
Mikhail Itkin <itkin.m@gmail.com>
2014-02-05 19:37:24 +0100

    [tests/netcdftime] dummy test for pycmbs/netcdftime
    Former-commit-id: 45cb2a2f1fdd2be4fe960992af6f7bc33c194f51

---
+++ pycmbs/tests/test_netcdftime.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+from pycmbs import netcdftime
+
+class TestPycmbsNetcdfTime(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_DummyTest(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+# vim: expandtab shiftwidth=4 softtabstop=4
