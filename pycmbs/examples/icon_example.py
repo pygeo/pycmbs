858449a9d291e84912d689deee843e05e8b47b0b
Alex Loew <a.loew@gmx.net>
2014-02-06 22:00:20 +0100

    [icon] adapted icon example
    Former-commit-id: 5a65517229fecfd740bf93e27aea0ab7fc1f905b
    Former-commit-id: 3cc9d56d43b9fd7a5a3d0a6bca5f720898972045

--- pycmbs/examples/icon_example.py
+++ pycmbs/examples/icon_example.py
@@ -1,21 +1,22 @@
-# implicit import
-# from pyCMBS import *
+from pycmbs.icon import Icon
+from pycmbs.plots import map_plot
 import matplotlib.pyplot as plt
 
 plt.close('all')
 
 #//// read data ////
-gridfile ='../example_data/icon/r2b4_amip.nc'
-datafile = '../example_data/icon/rms0006_atm_phy_DOM01_ML_0001.nc'
+gridfile ='../..//example_data/icon/r2b4_amip.nc'
+datafile = '../../example_data/icon/rms0006_atm_phy_DOM01_ML_0001.nc'
 
 #/// read data ///
 IC = Icon(datafile,gridfile,'rsns'); IC.read() #todo: read as standard for Data
-IC.label='shortwave net flux at surface'; IC.unit='$W/^2$'
+IC.label='shortwave net flux at surface'
+IC.unit='$W/^2$'
 
 #/// Do plotting ///
 print('Doing first plot ...')
-map_plot(IC,use_basemap=True)
+map_plot(IC, use_basemap=True)
 print('Doing second plot ...')
-map_plot(IC,use_basemap=False)
+map_plot(IC, use_basemap=False)
 
 plt.show()
