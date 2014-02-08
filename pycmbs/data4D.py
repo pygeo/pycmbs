403491c0f601fb693d6930810153979a95e615a9
Alex Loew <a.loew@gmx.net>
2014-02-08 15:35:47 +0100

    [data4D] pap8ify

--- pycmbs/data4D.py
+++ pycmbs/data4D.py
@@ -7,7 +7,7 @@ __date__ = "2013/04/16"
 __email__ = "walter.sauf@zmaw.de"
 
 '''
-# Copyright (C) 2013 
+# Copyright (C) 2013
 # Alexander Loew, alexander.loew@mpimet.mpg.de
 # See COPYING file for copying and redistribution conditions.
 #
@@ -25,13 +25,13 @@ __email__ = "walter.sauf@zmaw.de"
 '''
 Data4D CLASS
 class to work with the PFTs from JSBACH
-The Data4D class has the Data Class as a base class. 
-In addition to the Class Date has the Data4D class the attributs 
+The Data4D class has the Data Class as a base class.
+In addition to the Class Date has the Data4D class the attributs
 
   from_level  : the number of the lowest level, starting by 0
   to_level    : the number of the highest level
   data4D      : is an array of the attribude 'data' from the Data-Class
-  
+
 becase all data are stored in the data4D array, the normal data attribude is deleted.
 
 '''
@@ -46,54 +46,46 @@ from cdo import *
 
 class Data4D(Data):
 
-        
-  
 #-----------------------------------------------------------------------
-
-
-    def read(self,shift_lon,start_time=None,stop_time=None,time_var='time',checklat=True):
+    def read(self, shift_lon, start_time=None, stop_time=None, time_var='time', checklat=True):
         cdo = Cdo()
 
         if self.levellist == {}:
-	  level = 0
-	  L = cdo.showlevel(input='-selname,'+self.varname+' '+ self.filename )
-	  levell = L[0].split(' ')
-	  for k in levell:
-            self.levellist[int(k)] = level
-            level += 1
-            
-        self.level=int(self.levellist.get(self.levellist.keys()[0]))
-        
-        Data.read(self,shift_lon,start_time=start_time,stop_time=stop_time,time_var=time_var,checklat=checklat)
+            level = 0
+            L = cdo.showlevel(input='-selname,' + self.varname + ' ' + self.filename)
+            levell = L[0].split(' ')
+            for k in levell:
+                self.levellist[int(k)] = level
+                level += 1
 
-        
+        self.level = int(self.levellist.get(self.levellist.keys()[0]))
+
+        Data.read(self, shift_lon, start_time=start_time, stop_time=stop_time, time_var=time_var, checklat=checklat)
+
         del self.data
         for k in sorted(self.levellist.keys()):
-          self.level=int(self.levellist[int(k)])
-          self.data4D.append(Data.read_netcdf(self,self.varname))
-        
-          
-#-----------------------------------------------------------------------
+            self.level = int(self.levellist[int(k)])
+            self.data4D.append(Data.read_netcdf(self, self.varname))
 
+#-----------------------------------------------------------------------
     def _copy_Data4D_Info_to_Data(self):
         """
         copies all Data-attribut information from the Data4D type to the Data type.
         @return C{Data} object
         """
-        d = Data(None,None)
-
+        d = Data(None, None)
 
         for attr, value in self.__dict__.iteritems():
-	    if attr != 'data4D' and attr != 'levellist':
-#	      print " attr: " + attr + " value: "
-              try:
-                  #-copy (needed for arrays)
-                  cmd = "d." + attr + " = self." + attr + '.copy()'
-                  exec cmd
-              except:
-                  #-copy
-                  cmd = "d." + attr + " = self." + attr
-                  exec cmd
+            if attr != 'data4D' and attr != 'levellist':
+#         print " attr: " + attr + " value: "
+                try:
+                    #-copy (needed for arrays)
+                    cmd = "d." + attr + " = self." + attr + '.copy()'
+                    exec cmd
+                except:
+                    #-copy
+                    cmd = "d." + attr + " = self." + attr
+                    exec cmd
 
         return d
 
@@ -104,148 +96,147 @@ class Data4D(Data):
         copy complete C{Data4D} object including all attributes
         @return C{Data4D} object
         """
-        d = Data4D(None,None)
+        d = Data4D(None, None)
 #ws        print "copy of data4D.py"
 
-
         for attr, value in self.__dict__.iteritems():
-#	  print attr
-	  if not attr == "data4D":
-#	    print 'if'
-            try:
-                #-copy (needed for arrays)
-                cmd = "d." + attr + " = self." + attr + '.copy()'
-                exec cmd
+#     print attr
+            if not attr == "data4D":
+#       print 'if'
+                try:
+                    #-copy (needed for arrays)
+                    cmd = "d." + attr + " = self." + attr + '.copy()'
+                    exec cmd
 #                print "A: "+cmd
-            except:
-                #-copy
-                cmd = "d." + attr + " = self." + attr
-                exec cmd
+                except:
+                    #-copy
+                    cmd = "d." + attr + " = self." + attr
+                    exec cmd
 #                print "B: "+cmd
-          else:
-	   #print 'else '+ str(self.levellist.keys())
-	   #cmd = "d." + attr + " = self." + attr
-           exec cmd
-           for k in self.levellist.keys():
-#	     print 'K: '+str(k)
-	     d.data4D.append(self.data4D[self.levellist[k]].copy())
+            else:
+                #print 'else '+ str(self.levellist.keys())
+                #cmd = "d." + attr + " = self." + attr
+                exec cmd
+                for k in self.levellist.keys():
+#        print 'K: '+str(k)
+                    d.data4D.append(self.data4D[self.levellist[k]].copy())
 
         return d
 
-
 #-----------------------------------------------------------------------
-    def getDataFromLevel(self,l):
+    def getDataFromLevel(self, l):
         """
         returns a Data4D level as a Data object
         @param l level of data4D. Starting by 0:
         @return C{Data} object
         """
-        if self.levellist.has_key(int(l)):
-          ret = self._copy_Data4D_Info_to_Data()
-          d = self
-          #print int(l)
-          #print self.levellist[int(l)]
-          #print self.levellist
-          ret.data = d.data4D[self.levellist[int(l)]]
-          return ret
+        if int(l) in self.levellist.keys():
+            ret = self._copy_Data4D_Info_to_Data()
+            d = self
+            #print int(l)
+            #print self.levellist[int(l)]
+            #print self.levellist
+            ret.data = d.data4D[self.levellist[int(l)]]
+            return ret
         else:
-         raise ValueError, 'Given Level '+ str(l)+' not in data4D!'
-	  
+            raise ValueError('Given Level %s not in data4D!' % str(l))
 
 #-----------------------------------------------------------------------
-    def setDataFromLevel(self,da,l):
+    def setDataFromLevel(self, da, l):
         """
-        saves the date from da into an data4D lavel. 
-        The lebvel is given by parameter 'l'. 
-        
+        saves the date from da into an data4D lavel.
+        The lebvel is given by parameter 'l'.
+
         @param l level of Data4D. Starting by 0:
         @da  C{Data} object to be saved into Data4D
         """
-      
-        if self.levellist.has_key(int(l)):
-          d = self
-          d.data4D[self.levellist[int(l)]] = da.data
-        else:
-         raise ValueError, 'Given Level '+ str(l)+' not in data4D!'
-          
-        
-#-----------------------------------------------------------------------
-    def mulc(self,x,copy=True):
-      
-      if copy:
-        d = self.copy()
-      else:
-        d = self
 
-      for k in self.levellist.keys():
-        d.data4D[self.levellist[k]] *= x
-        
-      return d
+        if int(l) in self.levellist.keys():
+            d = self
+            d.data4D[self.levellist[int(l)]] = da.data
+        else:
+            raise ValueError('Given Level %s not in data4D!' % str(l))
+
 #-----------------------------------------------------------------------
-    def mul(self,x,copy=True):
-      
+    def mulc(self, x, copy=True):
+
         if copy:
             d = self.copy()
         else:
             d = self
 
         for k in self.levellist.keys():
-          if hasattr(x, 'data4D'):
-	    d.data4D[self.levellist[k]] *= x.data4D[self.levellist[k]]
-	  else:
-	    d.data4D[self.levellist[k]] *= x.data
-	  
+            d.data4D[self.levellist[k]] *= x
+
+        return d
+#-----------------------------------------------------------------------
+
+    def mul(self, x, copy=True):
+
+        if copy:
+            d = self.copy()
+        else:
+            d = self
+
+        for k in self.levellist.keys():
+            if hasattr(x, 'data4D'):
+                d.data4D[self.levellist[k]] *= x.data4D[self.levellist[k]]
+            else:
+                d.data4D[self.levellist[k]] *= x.data
+
 #        d.label = self.label + ' * ' + x.label
         return d
 #-----------------------------------------------------------------------
-    def divc(self,x,copy=True):
-      
-      if copy:
-          d = self.copy()
-      else:
-          d = self
 
-      for k in self.levellist.keys():
-        d.data4D[self.levellist[k]] /= x
-        
-      return d
+    def divc(self, x, copy=True):
 
-#-----------------------------------------------------------------------
-    def div(self,x,copy=True):
-      
         if copy:
             d = self.copy()
         else:
             d = self
 
         for k in self.levellist.keys():
-          if hasattr(x, 'data4D'):
-	    d.data4D[self.levellist[k]] /= x.data4D[self.levellist[k]]
-	  else:
-	    d.data4D[self.levellist[k]] /= x.data
-          
+            d.data4D[self.levellist[k]] /= x
+
         return d
 
 #-----------------------------------------------------------------------
-    def addc(self,x,copy=True):
-      
-      if copy:
-          d = self.copy()
-      else:
-          d = self
+    def div(self, x, copy=True):
 
-      for k in self.levellist.keys():
-        d.data4D[self.levellist[k]] += x
-        
-      return d
-#-----------------------------------------------------------------------
-    def add(self,x,copy=True):
-      
         if copy:
             d = self.copy()
         else:
             d = self
-            
+
+        for k in self.levellist.keys():
+            if hasattr(x, 'data4D'):
+                d.data4D[self.levellist[k]] /= x.data4D[self.levellist[k]]
+            else:
+                d.data4D[self.levellist[k]] /= x.data
+
+        return d
+
+#-----------------------------------------------------------------------
+    def addc(self, x, copy=True):
+
+        if copy:
+            d = self.copy()
+        else:
+            d = self
+
+        for k in self.levellist.keys():
+            d.data4D[self.levellist[k]] += x
+
+        return d
+#-----------------------------------------------------------------------
+
+    def add(self, x, copy=True):
+
+        if copy:
+            d = self.copy()
+        else:
+            d = self
+
         #print "01 copy self "+str(self.data4D[0][4,0,0])+" d "+str(d.data4D[0][4,0,0])
         #print "a1 copy self "+self.label+" d "+str(d.label)
         #print "a1 copy self "+self.label+" d "+str(d.label)
@@ -254,40 +245,41 @@ class Data4D(Data):
 #        print "a2 copy self "+self.label+" d "+str(d.label)
         for k in self.levellist.keys():
 #          print "02 copy self "+str(self.data4D[0][4,0,0])+" d "+str(d.data4D[0][4,0,0])
-          if hasattr(x, 'data4D'):
-	    d.data4D[self.levellist[k]] += x.data4D[self.levellist[k]]
-	  else:
-	    d.data4D[self.levellist[k]] += x.data
-          
+            if hasattr(x, 'data4D'):
+                d.data4D[self.levellist[k]] += x.data4D[self.levellist[k]]
+            else:
+                d.data4D[self.levellist[k]] += x.data
+
 #        print "03 copy self "+str(self.data4D[0][4,0,0])+" d "+str(d.data4D[0][4,0,0])
         return d
 
 #-----------------------------------------------------------------------
-    def subc(self,x,copy=True):
-      
-      if copy:
-          d = self.copy()
-      else:
-          d = self
+    def subc(self, x, copy=True):
 
-      for k in self.levellist.keys():
-        d.data4D[self.levellist[k]] -= x
-        
-      return d
-#-----------------------------------------------------------------------
-    def sub(self,x,copy=True):
-      
         if copy:
             d = self.copy()
         else:
             d = self
 
         for k in self.levellist.keys():
-          if hasattr(x, 'data4D'):
-	    d.data4D[self.levellist[k]] -= x.data4D[self.levellist[k]]
-	  else:
-	    d.data4D[self.levellist[k]] -= x.data
-          
+            d.data4D[self.levellist[k]] -= x
+
+        return d
+#-----------------------------------------------------------------------
+
+    def sub(self, x, copy=True):
+
+        if copy:
+            d = self.copy()
+        else:
+            d = self
+
+        for k in self.levellist.keys():
+            if hasattr(x, 'data4D'):
+                d.data4D[self.levellist[k]] -= x.data4D[self.levellist[k]]
+            else:
+                d.data4D[self.levellist[k]] -= x.data
+
         return d
 
 #-----------------------------------------------------------------------
@@ -296,20 +288,19 @@ class Data4D(Data):
         summates all Data4D levels to one level
         @return C{Data} object
         """
-      
+
         sum = self._copy_Data4D_Info_to_Data()
         d = self
-            
+
         sum.data = 0.0
-        
+
         for k in self.levellist.keys():
-          sum.data += self.data4D[self.levellist[k]]
-          
-          
+            sum.data += self.data4D[self.levellist[k]]
+
         return sum
 #-----------------------------------------------------------------------------------------------------------------------
 
-    def __init__(self,filename,varname,levellist=None,**kwargs):
+    def __init__(self, filename, varname, levellist=None, **kwargs):
         """
         Data4D class
 
@@ -321,15 +312,14 @@ class Data4D(Data):
 
         """
         self.levellist = {}
-        
-        if not levellist == None:
-	  level = 0
-	  for k in levellist:
-            self.levellist[int(k)] = level
-            level += 1
-	  
+
+        if not levellist is None:
+            level = 0
+            for k in levellist:
+                self.levellist[int(k)] = level
+                level += 1
+
         self.data4D = []
-        
-        Data.__init__(self,filename,varname, **kwargs)
-        self.mulc(self.scale_factor,copy=False)
-    
+
+        Data.__init__(self, filename, varname, **kwargs)
+        self.mulc(self.scale_factor, copy=False)
