5ac71b3ef73af9b7fd63145450f7e424ee3dd117
Alex Loew <a.loew@gmx.net>
2014-02-10 21:20:49 +0100

    [test_netcdf] added some tests for netcdf

--- pycmbs/tests/test_netcdf.py
+++ pycmbs/tests/test_netcdf.py
@@ -8,6 +8,19 @@ class TestPycmbsNetcdf(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_netCDFHandlerinit_WithInvalidValue(self):
+        with self.assertRaises(ValueError):
+            cdf = netcdf.NetCDFHandler(netcdf_backend='invalid_backend')
+
+    def test_netCDFHandlerinit_Default(self):
+        cdf = netcdf.NetCDFHandler()
+
+    def test_netCDFHandlerinit_Nio(self):
+        cdf = netcdf.NetCDFHandler(netcdf_backend='Nio')
+        self.assertEqual(cdf.type, 'Nio')  # could be that this fails if Nio not existing!
+        cdf = netcdf.NetCDFHandler(netcdf_backend='nio')
+        self.assertEqual(cdf.type, 'nio')
+
     def test_DummyTest(self):
         pass
 
