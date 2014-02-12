f19773bec4dd754f4fad806a103ea64cf716737f
Alex Loew <a.loew@gmx.net>
2014-02-12 16:37:33 +0100

    [doc] added two missing files to repo

---
+++ docsrc/source/examples/dummy.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+f = plt.figure()
+ax = f.add_subplot(111)
+ax. text(0.5,0.5,'This is a dummy figure')
+
+plt.show()
---
+++ docsrc/source/testplot.py
@@ -0,0 +1,13 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+f = plt.figure()
+ax = f.add_subplot(111)
+
+x = np.random.random(100)
+ax.plot(x)
+ax.set_title('This is a test plot')
+
+plt.show()
+
+
