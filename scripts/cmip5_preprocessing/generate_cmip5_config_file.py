d90e8e3ddb3e33e3bb0bbec0b47ab2a08f733d77
Al Lion <alexloew754@gmail.com>
2014-02-16 17:51:02 +0100

    [generator] initial upload of file for CMIP5 CFG generation script

---
+++ scripts/cmip5_preprocessing/generate_cmip5_config_file.py
@@ -0,0 +1,45 @@
+"""
+Generate a dummy configuration file that can be used
+for pycmbs processing of CMIP5 arechive data
+
+The script needs the path of the rootdirectory where the data is located.
+It then creates a list of model items like required by the *.cfg file.
+
+e.g.:
+BCC:bcc-csm1-1,CMIP5RAW,amip,/data/share/mpiles/.../PATH/TO/RAWDATA
+"""
+
+from pycmbs.benchmarking import preprocessor
+import os
+
+def main():
+
+    ofile = 'test.txt'
+    if os.path.exists(ofile):
+        os.remove(ofile)
+    o = open(ofile, 'w')
+
+    data_dir = '/data/share/mpiles/TRS/PROJECT_RESULTS/EvaClimod/CMIP5_RAWDATA_NEW/radiation/'
+    CMP = preprocessor.CMIP5ModelParser(data_dir)
+    model_list = CMP.get_all_models()
+
+    experiments = ['amip', 'historical']
+
+    for experiment in experiments:
+        o.write('\n')
+        o.write('##############################\n')
+        o.write('# ' + experiment + '\n')
+        o.write('##############################\n')
+        for institute in model_list.keys():
+            for model in model_list[institute]:
+                s = institute + ':' + model + ',' + 'CMIP5RAW' + experiment
+                print s
+
+    o.close()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
