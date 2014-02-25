1c990aeeb2a9275e287b8c141a2b1b6cda1cb9f3
mitkin <mitkin@mitkin-vb.(none)>
2014-02-25 23:00:07 +0100

    added license docstring - benchmarking

--- pycmbs/benchmarking/__init__.py
+++ pycmbs/benchmarking/__init__.py
@@ -1,3 +1,8 @@
+"""
+This file is part of pyCMBS. (c) 2012-2014
+For COPYING and LICENSE details, please refer to the files
+LICENSE.md and COPYRIGHT.md
+"""
 # XXX: implicit import
 # from benchmarking.utils import *
 from preprocessor import EnsemblePreprocessor, CMIP5Preprocessor
