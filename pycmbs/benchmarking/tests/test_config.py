16e873ac3476cf95a30d00d9306a75357932ffa5
Mikhail Itkin <itkin.m@gmail.com>
2014-02-28 15:49:15 +0100

    [tests/config] stub config file

---
+++ pycmbs/benchmarking/tests/test_config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+This file is part of pyCMBS. (c) 2012-2014
+For COPYING and LICENSE details, please refer to the file
+COPYRIGHT.md
+"""
+
+import unittest
+from pycmbs.benchmarking import config
+
+class TestPycmbsBenchmarkingReport(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_DummyTest(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+# vim: expandtab shiftwidth=4 softtabstop=4
