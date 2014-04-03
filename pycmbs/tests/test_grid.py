5a1d11683a9f62738a3310772bfe0b54140f3e22
Alex Loew <a.loew@gmx.net>
2014-04-03 16:09:49 +0200

    [test_grid] implemented tests for grid.py

--- pycmbs/tests/test_grid.py
+++ pycmbs/tests/test_grid.py
@@ -8,15 +8,46 @@ COPYRIGHT.md
 import unittest
 from pycmbs import grid
 
+from nose.tools import assert_raises
+import numpy as np
+
+
 class TestPycmbsGrid(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.lon = np.linspace(-120., 130.)
+        self.lat = np.linspace(-30., 30.)
+        self.grid  = grid.Grid(np.deg2rad(self.lat), np.deg2rad(self.lon), sphere_radius=6000000.)
 
     def test_DummyTest(self):
         pass
 
+    def test_grid_init(self):
+        with self.assertRaises(ValueError):
+            G = grid.Grid(np.deg2rad(self.lat), np.deg2rad(self.lon))
+
+        G = grid.Grid(np.deg2rad(self.lat), np.deg2rad(self.lon), sphere_radius=6000000.)
+
+    def test_grid_cellarea(self):
+        with self.assertRaises(ValueError):
+            self.grid.calc_cell_area()
+
+    def test_grid_plot(self):
+        self.grid.plot()
+
+    def test_grid_plot_voronoi(self):
+        self.grid.plot_voronoi()
+
+    def test_grid_plot_delaunay(self):
+        self.grid.plot_delaunay_grid()
+
+    def test_grid_draw_edge(self):
+        self.grid.draw_edges()
+
+
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
-
-# vim: expandtab shiftwidth=4 softtabstop=4
