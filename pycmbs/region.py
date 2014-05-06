a9c554e44cb1b4ec0f205361ed1377796e3feecf
Alex Loew <a.loew@gmx.net>
2014-05-06 19:55:31 +0200

    [region] added additional functionality for regional analysis

--- pycmbs/region.py
+++ pycmbs/region.py
@@ -5,6 +5,9 @@ For COPYING and LICENSE details, please refer to the file
 COPYRIGHT.md
 """
 
+import os
+import numpy as np
+
 
 class Region(object):
     """
@@ -16,6 +19,8 @@ class Region(object):
         """
         constructor of class
 
+        Parameters
+        ----------
         x1 : float
             start position in x-direction (either x index or longitude)
         x2 : float
@@ -33,64 +38,7 @@ class Region(object):
             the coordinates/indices
         """
 
-        if x2 < x1:
-            raise ValueError('Invalid X boundaries for region')
-        if y2 < y1:
-            raise ValueError('Invalid Y boundaries for region')
-
-        if type == 'index':  # coordinates are considered as inidces
-            self.x1 = x1
-            self.x2 = x2
-            self.y1 = y1
-            self.y2 = y2
-        elif type == 'latlon':
-            self.latmin = y1
-            self.latmax = y2
-            self.lonmin = x1
-            self.lonmax = x2
-
-        self.label = label
-        self.type = type
-        self.mask = mask
-
-    def _get_label(self):
-        return self.label.replace(' ', '')
-
-#-----------------------------------------------------------------------
-
-    def get_corners(self):
-        """
-        return a list of corner coordinates
-        (either indices or lat/lon, dependent on the data)
-
-        @return list of coordinates
-        """
-        if self.type == 'latlon':
-            return self._get_corners_latlon()
-        else:
-            return self._get_corners_index()
-
-#-----------------------------------------------------------------------
-
-    def _get_corners_latlon(self):
-        """
-        return a list of corner lat/lon
-        """
-        l = [(self.lonmin, self.latmin), (self.lonmin, self.latmax),
-             (self.lonmax, self.latmax), (self.lonmax, self.latmin)]
-        return l
-
-#-----------------------------------------------------------------------
-
-    def _get_corners_index(self):
-        """
-        return a list of corner indices
-        """
-        l = [(self.x1, self.y1), (self.x1, self.y2),
-             (self.x2, self.y2), (self.x2, self.y1)]
-        return l
-
-#-----------------------------------------------------------------------
+        raise ValueError('This class is depreciated! Use classes RegionPolygon, RegionIndex, RegionLatLon instead!')
 
     def get_subset(self, x):
         """
@@ -108,3 +56,184 @@ class Region(object):
         else:
             raise ValueError('Invalid data array for subsetting! %s'
                              % np.shape(x))
+
+
+class RegionGeneric(object):
+    """
+    Generic class to define regions
+    """
+    def __init__(self, id, label=None):
+        self.lon = None
+        self.lat = None
+        self.type = None
+        self.id = id
+        if label is None:
+            raise ValueError('ERROR: a label needs to be given for regions')
+        else:
+            self.label = label
+
+    def _check_bbox_validity(self, x1, x2, y1, y2):
+        if x2 < x1:
+            raise ValueError('Invalid X boundaries for region')
+        if y2 < y1:
+            raise ValueError('Invalid Y boundaries for region')
+
+    def _get_label(self):
+        return self.label.replace(' ', '')
+
+    def get_Bbox(self):
+        """
+        return bounding box of coordinates
+        """
+        if self.type == 'index':
+            return self._get_corners_index()
+        else:
+            return self._get_corners_latlon()
+
+    def _get_corners_index(self):
+        """ return a list of corner indices """
+        l = [(self.x1, self.y1), (self.x1, self.y2),
+             (self.x2, self.y2), (self.x2, self.y1)]
+        return l
+
+    def _get_corners_latlon(self):
+        """ return a list of corner lat/lon """
+        if self.lat is None:
+            raise ValueError('ERROR: can not retrieve Bbox without latitudes!')
+        if self.lon is None:
+            raise ValueError('ERROR: can not retrieve Bbox without longitudes!')
+
+        l = [(min(self.lon), min(self.lat)), (min(self.lon), max(self.lat)),
+             (max(self.lon), max(self.lat)), (max(self.lon), min(self.lat))]
+        return l
+
+
+class RegionPolygon(RegionGeneric):
+    """
+    define a region by a polygon
+    """
+    def __init__(self, id, lon, lat, label=None):
+        super(RegionPolygon, self).__init__(id, label=label)
+        self.lon = lon
+        self.lat = lat
+        self.type = 'polygon'
+
+
+class RegionIndex(RegionGeneric):
+    def __init__(self, id, x1, x2, y1, y2, label=None):
+        self._check_bbox_validity(x1, x2, y1, y2)
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        super(RegionIndex, self).__init__(id, label=label)
+        self.type = 'index'
+
+
+class RegionBboxLatLon(RegionPolygon):
+    """
+    specify region through a Bounding box using
+    lat/lon coordinates
+    """
+    def __init__(self, id, x1, x2, y1, y2, label=None):
+        self._check_bbox_validity(x1, x2, y1, y2)
+        self.latmin = y1
+        self.latmax = y2
+        self.lonmin = x1
+        self.lonmax = x2
+        lon, lat = self._bbox_to_points()  # create vector with coordinates
+        super(RegionBboxLatLon, self).__init__(id, lon, lat, label=label)
+
+    def _bbox_to_points(self):
+        """ write Bbox corners to sequence of coordinates of points """
+        lon = np.asarray([self.lonmin, self.lonmin, self.lonmax, self.lonmax])
+        lat = np.asarray([self.latmin, self.latmax, self.latmax, self.latmin])
+        return lon, lat
+
+
+class RegionParser(object):
+    """
+    class to parse region definitions in differnt formats
+    reads regions from files and provides them in a list of  Region
+    objects
+    """
+
+    def __init__(self, filename, format='ini'):
+        """
+        Parameters
+        ----------
+        filename : str
+            name of file to read
+        format : str
+            data format of region file ['ini']
+            ini : windows like INI file structure
+        """
+        self.format = format
+        self.filename = filename
+
+        self._check()
+
+        self.regions = {}
+        self._read()
+
+    def _check(self):
+        if not os.path.exists(self.filename):
+            raise ValueError('ERROR: Regionfile not existing!')
+        if self.format not in ['ini']:
+            raise ValueError('ERROR: Format not correctly specified!')
+
+    def _read(self):
+        if self.format == 'ini':
+            self._parse_ini_file()
+        else:
+            raise ValueError('ERROR: invalid format!')
+
+    def _parse_ini_file(self):
+        """
+        parse a region file that has the structure of a Windows INI file
+        """
+        from ConfigParser import SafeConfigParser
+        parser = SafeConfigParser()
+        parser.read(self.filename)
+
+        for section in parser.sections():
+            if section.upper() == 'COMMENT':
+                for name, value in parser.items(section):
+                    if name.upper() == 'LABEL':
+                        self.label = value
+                    elif name.upper() == 'DESCRIPTION':
+                        self.description = value
+                    else:
+                        raise ValueError('ERROR: Invalid option for COMMENT')
+            else:  # read region sections
+                tmp_lon = None
+                tmp_lat = None
+                tmp_id = None
+                for name, value in parser.items(section):
+
+                    if name.upper() == 'COORDINATES':
+                        # the coordinates come as a list of tuples. Split them
+                        # into lat/lon
+                        coord = eval(value)
+                        lat = []
+                        lon = []
+                        for la, lo in coord:
+                            lat.append(la)
+                            lon.append(lo)
+                        tmp_lat = np.asarray(lat)
+                        tmp_lon = np.asarray(lon)
+                    elif name.upper() == 'ID':
+                        tmp_id = int(value)
+                    else:
+                        print section, name
+                        raise ValueError('Unsupported attribute!')
+
+                if tmp_lon is None:
+                    raise ValueError('ERROR: no longitudes found!')
+                if tmp_lat is None:
+                    raise ValueError('ERROR: no latitudes found!')
+                if tmp_id is None:
+                    raise ValueError('ERROR: no ID specified!')
+
+                self.regions.update({section: RegionPolygon(tmp_id, tmp_lon, tmp_lat, label=section)})
+                del tmp_lon, tmp_lat, tmp_id
