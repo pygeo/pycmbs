09fdcabe0b1184a185a5e01acdd4a81f51e45f49
Alex Loew <a.loew@gmx.net>
2014-06-05 23:07:01 +0200

    [test_cdo] adapted to tempfile

--- pycmbs/tests/test_cdo.py
+++ pycmbs/tests/test_cdo.py
@@ -13,16 +13,19 @@ from cdo import Cdo
 from pycmbs.examples import download
 import os
 
+import tempfile
+
 class TestData(unittest.TestCase):
     def setUp(self):
         self.D = download.get_sample_file(name='air')
         self.file = download.get_sample_file(name='air', return_object=False)  # filename only
         self.areafile = self.file[:-3] + '_cell_area.nc'
+        self._tmpdir = tempfile.mkdtemp()
 
     def test_cdo_general(self):
         # test if cdos work in general
         cdo = Cdo()
-        out_file = 'cdo_test.nc'
+        out_file = self._tmpdir + os.sep + 'cdo_test.nc'
         if os.path.exists(out_file):
             os.remove(out_file)
         cdo.timmean(options='-f nc', output=out_file, input=self.file)
