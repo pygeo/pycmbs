13a48d8705a0672e1624ca171ae2cadbceb1f8e3
Alex Loew <a.loew@gmx.net>
2014-07-29 11:43:39 +0200

    [test_region] implemented test for writing a shapefile

--- pycmbs/tests/test_region.py
+++ pycmbs/tests/test_region.py
@@ -2,6 +2,8 @@
 
 import unittest
 from pycmbs import region
+import tempfile
+import os
 
 class TestRegion(unittest.TestCase):
 
@@ -11,7 +13,34 @@ class TestRegion(unittest.TestCase):
     def test_DummyTest(self):
         pass
 
+    def test_shape_write(self):
+        # example taken from pyshp examples: https://code.google.com/p/pyshp/
+        import shapefile
+
+        # Make a point shapefile
+        w = shapefile.Writer(shapefile.POINT)
+        w.point(90.3, 30)
+        w.point(92, 40)
+        w.point(-122.4, 30)
+        w.point(-90, 35.1)
+        w.field('FIRST_FLD')
+        w.field('SECOND_FLD','C','40')
+        w.record('First','Point')
+        w.record('Second','Point')
+        w.record('Third','Point')
+        w.record('Fourth','Point')
+        w.save('shapefiles/test/point')
+        # Create a polygon shapefile
+        w = shapefile.Writer(shapefile.POLYGON)
+        w.poly(parts=[[[1,5],[5,5],[5,1],[3,3],[1,1]]])
+        w.field('FIRST_FLD','C','40')
+        w.field('SECOND_FLD','C','40')
+        w.record('First','Polygon')
+        w.save(tempfile.mkdtemp() + os.sep + 'mytestshape')
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
 
-# vim: expandtab shiftwidth=4 softtabstop=4
