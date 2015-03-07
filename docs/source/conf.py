b1341cba8b0719bb0792ba94d0c0099953431385
Alex Loew <a.loew@gmx.net>
2015-03-07 19:03:36 +0100

    deactivated matplotlib extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,9 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../../'))
-sys.path.append(os.path.abspath('sphinxext'))
+
+# sys.path.insert(0, os.path.abspath('../../'))
+# sys.path.append(os.path.abspath('sphinxext'))
 
 
 
@@ -31,15 +32,17 @@ sys.path.append(os.path.abspath('sphinxext'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-          'matplotlib.sphinxext.mathmpl',
-          'matplotlib.sphinxext.only_directives',
-          'matplotlib.sphinxext.plot_directive',
-          'matplotlib.sphinxext.ipython_directive',
-          'sphinx.ext.autodoc',
-          'sphinx.ext.doctest',
-          'ipython_console_highlighting',
-          'numpydoc']
+
+#~ extensions = [
+          #~ 'matplotlib.sphinxext.mathmpl',
+          #~ 'matplotlib.sphinxext.only_directives',
+          #~ 'matplotlib.sphinxext.plot_directive',
+          #~ 'matplotlib.sphinxext.ipython_directive',
+          #~ 'sphinx.ext.autodoc',
+          #~ 'sphinx.ext.doctest',
+          #~ 'ipython_console_highlighting',
+          #~ 'inheritance_diagram',
+          #~ 'numpydoc']
 
 
 # Add any paths that contain templates here, relative to this directory.
