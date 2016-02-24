c90cb14f7b4f8bfb4fb215fc0064fb9b624afd3e
Alex Loew <a.loew@gmx.net>
2016-02-24 11:40:49 +0100

    moved major parts of DATA and statistic to GEOVAL project

--- pycmbs/statistic/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-This file is part of pyCMBS.
-(c) 2012- Alexander Loew
-For COPYING and LICENSE details, please refer to the LICENSE file
-"""
-
-from statistic_basic import *
-from lomb_scargle import *
